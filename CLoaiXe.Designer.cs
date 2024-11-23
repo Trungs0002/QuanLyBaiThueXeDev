32398310f89cdf7665b4a3ed6dfd2edf94da9c7a
2374801030072 - Đào Đức Trung - 71K29KTPM02 <daoductrung0512@gmail.com>
2024-11-24 00:20:35 +0700

    Add CLoaiXe

---
+++ CLoaiXe.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace QuanLyBaiThueXeDev
+{
+    partial class CLoaiXe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtGridViewLoaiXe = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtMaLoaiXe = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenLoai = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHangSanXuat = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNamSanXuat = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMoTa = new System.Windows.Forms.TextBox();
+            this.btnThemLoaiXe = new System.Windows.Forms.Button();
+            this.btnSuaLoaiXe = new System.Windows.Forms.Button();
+            this.btnXoaLoaiXe = new System.Windows.Forms.Button();
+            this.btnTimKiemLoaiXe = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridViewLoaiXe)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtGridViewLoaiXe
+            // 
+            this.dtGridViewLoaiXe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGridViewLoaiXe.Location = new System.Drawing.Point(12, 267);
+            this.dtGridViewLoaiXe.Name = "dtGridViewLoaiXe";
+            this.dtGridViewLoaiXe.Size = new System.Drawing.Size(776, 150);
+            this.dtGridViewLoaiXe.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnXoaLoaiXe);
+            this.panel1.Controls.Add(this.btnSuaLoaiXe);
+            this.panel1.Controls.Add(this.btnThemLoaiXe);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtMoTa);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtNamSanXuat);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtHangSanXuat);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtTenLoai);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtMaLoaiXe);
+            this.panel1.Location = new System.Drawing.Point(12, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 171);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtMaLoaiXe
+            // 
+            this.txtMaLoaiXe.Location = new System.Drawing.Point(103, 31);
+            this.txtMaLoaiXe.Name = "txtMaLoaiXe";
+            this.txtMaLoaiXe.Size = new System.Drawing.Size(147, 20);
+            this.txtMaLoaiXe.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "txtMaLoaiXe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "txtTenLoai";
+            // 
+            // txtTenLoai
+            // 
+            this.txtTenLoai.Location = new System.Drawing.Point(103, 69);
+            this.txtTenLoai.Name = "txtTenLoai";
+            this.txtTenLoai.Size = new System.Drawing.Size(147, 20);
+            this.txtTenLoai.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(277, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "txtHangSanXuat";
+            // 
+            // txtHangSanXuat
+            // 
+            this.txtHangSanXuat.Location = new System.Drawing.Point(349, 31);
+            this.txtHangSanXuat.Name = "txtHangSanXuat";
+            this.txtHangSanXuat.Size = new System.Drawing.Size(147, 20);
+            this.txtHangSanXuat.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(277, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "txtNamSanXuat";
+            // 
+            // txtNamSanXuat
+            // 
+            this.txtNamSanXuat.Location = new System.Drawing.Point(349, 69);
+            this.txtNamSanXuat.Name = "txtNamSanXuat";
+            this.txtNamSanXuat.Size = new System.Drawing.Size(147, 20);
+            this.txtNamSanXuat.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(533, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "txtMoTa";
+            // 
+            // txtMoTa
+            // 
+            this.txtMoTa.Location = new System.Drawing.Point(605, 56);
+            this.txtMoTa.Name = "txtMoTa";
+            this.txtMoTa.Size = new System.Drawing.Size(147, 20);
+            this.txtMoTa.TabIndex = 8;
+            // 
+            // btnThemLoaiXe
+            // 
+            this.btnThemLoaiXe.Location = new System.Drawing.Point(34, 110);
+            this.btnThemLoaiXe.Name = "btnThemLoaiXe";
+            this.btnThemLoaiXe.Size = new System.Drawing.Size(75, 23);
+            this.btnThemLoaiXe.TabIndex = 10;
+            this.btnThemLoaiXe.Text = "btnThemLoaiXe";
+            this.btnThemLoaiXe.UseVisualStyleBackColor = true;
+            // 
+            // btnSuaLoaiXe
+            // 
+            this.btnSuaLoaiXe.Location = new System.Drawing.Point(128, 110);
+            this.btnSuaLoaiXe.Name = "btnSuaLoaiXe";
+            this.btnSuaLoaiXe.Size = new System.Drawing.Size(75, 23);
+            this.btnSuaLoaiXe.TabIndex = 11;
+            this.btnSuaLoaiXe.Text = "btnSuaLoaiXe";
+            this.btnSuaLoaiXe.UseVisualStyleBackColor = true;
+            // 
+            // btnXoaLoaiXe
+            // 
+            this.btnXoaLoaiXe.Location = new System.Drawing.Point(222, 110);
+            this.btnXoaLoaiXe.Name = "btnXoaLoaiXe";
+            this.btnXoaLoaiXe.Size = new System.Drawing.Size(75, 23);
+            this.btnXoaLoaiXe.TabIndex = 12;
+            this.btnXoaLoaiXe.Text = "btnXoaLoaiXe";
+            this.btnXoaLoaiXe.UseVisualStyleBackColor = true;
+            // 
+            // btnTimKiemLoaiXe
+            // 
+            this.btnTimKiemLoaiXe.Location = new System.Drawing.Point(676, 15);
+            this.btnTimKiemLoaiXe.Name = "btnTimKiemLoaiXe";
+            this.btnTimKiemLoaiXe.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiemLoaiXe.TabIndex = 13;
+            this.btnTimKiemLoaiXe.Text = "btnTimKiemLoaiXe";
+            this.btnTimKiemLoaiXe.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtTimKiem);
+            this.panel2.Controls.Add(this.btnTimKiemLoaiXe);
+            this.panel2.Location = new System.Drawing.Point(13, 209);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(775, 52);
+            this.panel2.TabIndex = 2;
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(33, 15);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(619, 20);
+            this.txtTimKiem.TabIndex = 14;
+            // 
+            // CLoaiXe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dtGridViewLoaiXe);
+            this.Name = "CLoaiXe";
+            this.Text = "CLoaiXe";
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridViewLoaiXe)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtGridViewLoaiXe;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnXoaLoaiXe;
+        private System.Windows.Forms.Button btnSuaLoaiXe;
+        private System.Windows.Forms.Button btnThemLoaiXe;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMoTa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNamSanXuat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHangSanXuat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenLoai;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaLoaiXe;
+        private System.Windows.Forms.Button btnTimKiemLoaiXe;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtTimKiem;
+    }
+}
--- CLoaiXe.cs
+++ CLoaiXe.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QuanLyBaiThueXeDev
 {
-    public partial class Form1 : Form
+    public partial class CLoaiXe : Form
     {
-        public Form1()
+        public CLoaiXe()
         {
             InitializeComponent();
         }
