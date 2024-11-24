58a1f9e71e7713c9c6903d95fb1a7adafc4ec0b2
2374801030072 - Đào Đức Trung - 71K29KTPM02 <daoductrung0512@gmail.com>
2024-11-24 19:55:48 +0700

    Add FDoanhThu

---
+++ View/FDoanhThu.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace QuanLyBaiThueXeDev.View
+{
+    partial class FDoanhThu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.dtGridViewDoanhThu = new System.Windows.Forms.DataGridView();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridViewDoanhThu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtTimKiem);
+            this.panel2.Controls.Add(this.btnTimKiem);
+            this.panel2.Location = new System.Drawing.Point(13, 210);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(775, 52);
+            this.panel2.TabIndex = 11;
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(33, 15);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(619, 20);
+            this.txtTimKiem.TabIndex = 14;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(676, 15);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiem.TabIndex = 13;
+            this.btnTimKiem.Text = "btnTimKiem";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            // 
+            // dtGridViewDoanhThu
+            // 
+            this.dtGridViewDoanhThu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGridViewDoanhThu.Location = new System.Drawing.Point(12, 268);
+            this.dtGridViewDoanhThu.Name = "dtGridViewDoanhThu";
+            this.dtGridViewDoanhThu.Size = new System.Drawing.Size(776, 150);
+            this.dtGridViewDoanhThu.TabIndex = 9;
+            // 
+            // FDoanhThu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dtGridViewDoanhThu);
+            this.Name = "FDoanhThu";
+            this.Text = "FDoanhThu";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridViewDoanhThu)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.DataGridView dtGridViewDoanhThu;
+    }
+}
