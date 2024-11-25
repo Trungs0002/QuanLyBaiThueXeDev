5f70815d46a2da7eb1a760148be5836ee0de06a2
2374801030072 - Đào Đức Trung - 71K29KTPM02 <daoductrung0512@gmail.com>
2024-11-25 23:42:28 +0700

    Revert "test update"
    This reverts commit 6688034ff7f1f210f5106ade14467b71bdf986da.

--- View/FDoanhThu.Designer.cs
+++ View/FDoanhThu.Designer.cs
@@ -32,7 +32,6 @@
             this.txtTimKiem = new System.Windows.Forms.TextBox();
             this.btnTimKiem = new System.Windows.Forms.Button();
             this.dtGridViewDoanhThu = new System.Windows.Forms.DataGridView();
-            this.lblTongDoanhThu = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dtGridViewDoanhThu)).BeginInit();
             this.SuspendLayout();
@@ -41,26 +40,23 @@
             // 
             this.panel2.Controls.Add(this.txtTimKiem);
             this.panel2.Controls.Add(this.btnTimKiem);
-            this.panel2.Location = new System.Drawing.Point(17, 258);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(13, 210);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1033, 64);
+            this.panel2.Size = new System.Drawing.Size(775, 52);
             this.panel2.TabIndex = 11;
             // 
             // txtTimKiem
             // 
-            this.txtTimKiem.Location = new System.Drawing.Point(44, 18);
-            this.txtTimKiem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTimKiem.Location = new System.Drawing.Point(33, 15);
             this.txtTimKiem.Name = "txtTimKiem";
-            this.txtTimKiem.Size = new System.Drawing.Size(824, 22);
+            this.txtTimKiem.Size = new System.Drawing.Size(619, 20);
             this.txtTimKiem.TabIndex = 14;
             // 
             // btnTimKiem
             // 
-            this.btnTimKiem.Location = new System.Drawing.Point(901, 18);
-            this.btnTimKiem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTimKiem.Location = new System.Drawing.Point(676, 15);
             this.btnTimKiem.Name = "btnTimKiem";
-            this.btnTimKiem.Size = new System.Drawing.Size(100, 28);
+            this.btnTimKiem.Size = new System.Drawing.Size(75, 23);
             this.btnTimKiem.TabIndex = 13;
             this.btnTimKiem.Text = "btnTimKiem";
             this.btnTimKiem.UseVisualStyleBackColor = true;
@@ -68,30 +64,19 @@
             // dtGridViewDoanhThu
             // 
             this.dtGridViewDoanhThu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtGridViewDoanhThu.Location = new System.Drawing.Point(16, 330);
-            this.dtGridViewDoanhThu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtGridViewDoanhThu.Location = new System.Drawing.Point(12, 268);
             this.dtGridViewDoanhThu.Name = "dtGridViewDoanhThu";
-            this.dtGridViewDoanhThu.RowHeadersWidth = 51;
-            this.dtGridViewDoanhThu.Size = new System.Drawing.Size(1035, 185);
+            this.dtGridViewDoanhThu.Size = new System.Drawing.Size(776, 150);
             this.dtGridViewDoanhThu.TabIndex = 9;
-            // 
-            // lblTongDoanhThu
-            // 
-            this.lblTongDoanhThu.Location = new System.Drawing.Point(612, 191);
-            this.lblTongDoanhThu.Name = "lblTongDoanhThu";
-            this.lblTongDoanhThu.Size = new System.Drawing.Size(100, 22);
-            this.lblTongDoanhThu.TabIndex = 12;
             // 
             // FDoanhThu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.lblTongDoanhThu);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.dtGridViewDoanhThu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FDoanhThu";
             this.Text = "FDoanhThu";
             this.Load += new System.EventHandler(this.FDoanhThu_Load);
@@ -99,7 +84,6 @@
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dtGridViewDoanhThu)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -109,6 +93,5 @@
         private System.Windows.Forms.TextBox txtTimKiem;
         private System.Windows.Forms.Button btnTimKiem;
         private System.Windows.Forms.DataGridView dtGridViewDoanhThu;
-        private System.Windows.Forms.TextBox lblTongDoanhThu;
     }
 }
--- View/FDoanhThu.cs
+++ View/FDoanhThu.cs
@@ -1,5 +1,4 @@
-﻿using QuanLyBaiThueXeDev.Ctrl_QLBTX;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,8 +12,6 @@ namespace QuanLyBaiThueXeDev.View
 {
     public partial class FDoanhThu : Form
     {
-        private Ctrl_PhieuThue ctrlPhieuThue = new Ctrl_PhieuThue();
-        private Ctrl_LoaiXe ctrlXe = new Ctrl_LoaiXe();
         public FDoanhThu()
         {
             InitializeComponent();
@@ -24,34 +21,5 @@ namespace QuanLyBaiThueXeDev.View
         {
             this.ControlBox = false;
         }
-
-        private void LoadDoanhThuThang(int thang, int nam)
-        {
-            var phieuThueTrongThang = ctrlPhieuThue.findAll()
-                .Where(pt => pt.NgayThue.HasValue &&
-                             pt.NgayThue.Value.Month == thang &&
-                             pt.NgayThue.Value.Year == nam)
-                .ToList();
-
-            var doanhThu = phieuThueTrongThang.Sum(pt => pt.SoNgayMuon * pt.DonGia);
-
-            // Hiển thị trong DataGridView
-            dtGridViewDoanhThu.DataSource = phieuThueTrongThang.Select(pt => new
-            {
-                pt.SoPhieuThue,
-                pt.NgayThue,
-                pt.MaKhachHang,
-                pt.BienSoXe,
-                pt.SoNgayMuon,
-                pt.DonGia,
-                TongTien = pt.SoNgayMuon * pt.DonGia
-            }).ToList();
-
-            // Hiển thị tổng doanh thu
-            lblTongDoanhThu.Text = $"Tổng Doanh Thu: {doanhThu:C}";
-        }
-
-        
-
     }
 }
